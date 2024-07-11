74ad62a5a9c1411eea05862e82f4787d733fa2e4
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-07-12 05:39:39 +0900

    コンパイル設定

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.config.node = {
+    __dirname: false,
+    __filename: false,
+    global: false
+  }
+
 module.exports = environment.toWebpackConfig()
