4419ece9cd532297b56682dafb2dbaa6ea86ad04
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-07-11 21:28:01 +0900

    環境変数設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 require 'capybara/rspec'
 
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
