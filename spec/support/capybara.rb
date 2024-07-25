5a3f87db480ae23f1b476bc146460ebabd968420
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-07-25 14:43:19 +0900

    環境変数追加

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -26,6 +26,5 @@ Capybara.register_driver :remote_chrome do |app|
   options.add_argument('headless')
   options.add_argument('disable-gpu')
   options.add_argument('window-size=1680,1050')
-
-Capybara::Selenium::Driver.new(app, browser: :remote, url: url, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, options: options)
 end
