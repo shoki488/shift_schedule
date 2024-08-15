f71ba0f892478daa16f54eaa84b3f4bd91e7870d
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-08-15 21:42:33 +0900

    エラー確認

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,10 +47,9 @@ RSpec.configure do |config|
       options.add_argument('--disable-gpu')
       options.add_argument('--no-sandbox')
       options.add_argument('--disable-dev-shm-usage')
-    
       Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
     end
-    
+
     Capybara.javascript_driver = :headless_chrome
   end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
