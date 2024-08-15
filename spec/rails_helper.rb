11a58e7a81b8e1bf312e899b2a2095a177922ed9
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-08-15 19:02:03 +0900

    draversの設定追記

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,19 @@ RSpec.configure do |config|
       Selenium::WebDriver::Chrome.driver_path = "/path/to/your/chromedriver"
     end
   end
+  
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1366,720')
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  Capybara.javascript_driver = :selenium_chrome_headless
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures')
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
