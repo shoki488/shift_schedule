3983ce357de1b119ee6048d7a02c82f9df50e532
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-08-21 18:15:33 +0900

    composeファイル設定変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,17 @@
+Capybara.register_driver :headless_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1920,1080')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :headless_chrome
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome
@@ -15,17 +29,3 @@ RSpec.configure do |config|
     end
   end
 end
-
-Capybara.register_driver :headless_chrome do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.binary = ENV.fetch('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome-stable')
-  options.add_argument('--headless')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1920,1080')
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-Capybara.javascript_driver = :headless_chrome
