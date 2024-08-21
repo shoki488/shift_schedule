85282a0bfcda0576f2255fad576442b2af9a5de7
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-08-21 19:00:37 +0900

    ファイル設定変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,13 +1,15 @@
+require 'selenium/webdriver'
+
 Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
-
+  options.binary = ENV.fetch('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
   options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1920,1080')
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, driver_path: ENV.fetch('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver'))
 end
 
 Capybara.javascript_driver = :headless_chrome
