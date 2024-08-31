d90271d71e230f918bacec581955b9d11b98183d
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-09-01 07:31:34 +0900

    Fix Webpack configuration for turbo-rails

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,12 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
+const path = require('path')
 
 environment.config.node = {
   __dirname: true,
   __filename: true,
   global: true
-};
-environment.config.resolve.alias['@hotwired'] = 'node_modules/@hotwired'
-module.exports = environment;
+}
+
+environment.config.resolve.alias['@hotwired'] = path.resolve(__dirname, '../../node_modules/@hotwired')
+
+module.exports = environment
