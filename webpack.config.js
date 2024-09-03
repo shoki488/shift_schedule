2dafeb434ad8b995da9537d6fba94aea13db0cff
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-09-03 16:04:21 +0900

    fix babel error

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const { environment } = require('@rails/webpacker');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
   },
   module: {
     rules: [
@@ -13,9 +17,24 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-syntax-dynamic-import']
+          }
         }
       }
     ]
   }
 };
+
+// 古いプロパティ削除の試みを取り除く
+// environment.config.delete('node.dgram');
+// environment.config.delete('node.fs');
+// environment.config.delete('node.net');
+// environment.config.delete('node.tls');
+// environment.config.delete('node.child_process');
+
+environment.config.merge(customConfig);
+
+module.exports = environment.toWebpackConfig();
