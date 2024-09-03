50cc27488b353eb5d3ff963d42888ce25f9219eb
真嶌勝規 <mashimakatsunori@mashimakatsunorinoMacBook-Air.local>
2024-09-03 15:28:28 +0900

    Update Node.js version and fix Webpack configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const path = require('path')
-
-environment.config.merge({
-  resolve: {
-    alias: {
-      '@hotwired': path.resolve(__dirname, '../../node_modules/@hotwired')
-    }
-  }
-})
 
 module.exports = environment
